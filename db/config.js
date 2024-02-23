1a9f2eda5c30c0d183c8fb4e21c1cf6a922bde4c
dsanchez-2018471 <dsanchez-2018471@kinal.edu.gt>
2024-02-23 08:37:42 -0600

    Actualizacion DB

--- db/config.js
+++ db/config.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const dbConnection = async () => {
     try {
         await mongoose.connect(process.env.MONGODB_CNN,{});
-        console.log('base de datos conectada');
+        console.log('Coneccion establecida con la base de datos');
     } catch (e) {
         throw new Error('Error al conectar a la base de datos', e);   
     }
