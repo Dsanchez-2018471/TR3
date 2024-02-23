4228e3a5f575b172c7a06231d97badd7b98f2684
dsanchez-2018471 <dsanchez-2018471@kinal.edu.gt>
2024-02-23 08:43:43 -0600

    Validacion de datos en helpers

--- helpers/db-validators.js
+++ helpers/db-validators.js
@@ -5,28 +5,28 @@ const Animal = require('../models/animal');
 const esRoleValido = async (role = '') => {
     const existeRol = await Role.findOne({ role });
     if (!existeRol) {
-        throw new Error(`El rol ${role} no existe en la base de datos`);
+        throw new Error(`Este rol ${role} no existe en la base de datos`);
     }
 }
 
 const existenteEmail = async (correo = '') => {
     const existeEmail = await Usuario.findOne({ correo });
     if (existeEmail) {
-        throw new Error(`El correo ${correo} ya esta registrado`);
+        throw new Error(`El correo ${correo} ya se encuentra registrado`);
     }
 }
 
 const existeUsuarioById = async (id = '') => {
     const existeUsuario = await Usuario.findOne({id});
     if(existeUsuario){
-        throw new Error(`El usuario con el ${ id } no existe`)
+        throw new Error(`El usuario con el ${ id } no existe en la base de datos`)
     }
 }
 
 const existeAnimalById = async (id = '') => {
     const existeAnimal = await Animal.findOne({id});
     if(existeAnimal){
-        throw new Error(`El animal con el ${ id } no existe`)
+        throw new Error(`El animal con el ${ id } no existe en la base de datos`)
     }
 }
 
