60b09869706ebe4b3fe64308bf49a8b93f248a01
Dsanchez-2018471 <dsanchez-2018471@kinal.edu.gt>
2024-03-03 16:11:44 -0600

    Se agregan distintos archivos

--- src/README.md
+++ src/README.md
@@ -5,3 +5,4 @@
 >>>>>>> 64691617361d959c1a8facbec45a62804cc2cd66
 "laboratorio-2tr" 
 "# Laboratorio-2TRPMA" 
+"# TR3" 
---
+++ src/empresas/empresas.routes.js
@@ -0,0 +1,38 @@
+import { Router } from 'express';
+import { check } from 'express-validator';
+import { empresaPost, empresaGet, empresaPut } from './empresas.controller.js';
+import { existenteEmpresa, existeEmpresaById } from '../helpers/db-validators.js';
+import { validarCampos } from '../middlewares/validar-campos.js';
+import { validarJWT } from '../middlewares/validar-jwt.js';
+
+
+
+const router = new Router();
+
+router.get('/', empresaGet);
+
+router.post(
+  '/',
+  [
+    check('nombreE').custom(existenteEmpresa),
+    check('nivelImpacto', 'El nivel de impacto es obligatorio').not().isEmpty(),
+    check('años', 'Los años de la empresa son obligatorios').not().isEmpty(),
+    validarCampos,
+  ],
+  empresaPost
+);
+
+router.get("/:order", [validarJWT, validarCampos], empresaGet);
+// Funcion por si no se manda la orden
+router.get("/", [validarJWT, validarCampos], empresaGet);
+
+router.put(
+  "/:id",
+  [
+      check("id", "Este id no es un formato válido de MongoDB").isMongoId(),
+      check("id").custom(existeEmpresaById),
+      validarCampos
+  ], empresaPut);
+
+
+export default router;
